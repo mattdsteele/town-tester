0fb6e784904e9388ddee1f6a8eb915e27c86709f
Matt Steele <orphum@gmail.com>
2012-09-16 18:37:09 -0500

    Add statistics generation & printing by language

--- lib/repo.rb
+++ lib/repo.rb
@@ -3,9 +3,35 @@ class Stats
     @repos = repos
   end
   def all_languages
-    @repos.select {|i| i.tests}.length / @repos.length
+    ratio @repos
+  end
+
+  def languages
+    @repos.map {|i| i.language}.uniq
+  end
+
+  def language(l)
+    by_language = @repos.select {|i| i.language == l}
+    ratio by_language
+  end
+
+  def ratio(repos)
+    repos.select {|i| i.tests}.length / repos.length.to_f
+  end
+
+  def print_stats
+    language_stats = languages.map do |l|
+      { :language => l, :ratio => language(l) }
+    end
+
+    output = ""
+    language_stats.each do |l|
+      output << "#{l[:language]}: #{(l[:ratio] * 100).round}%\n"
+    end
+    output
   end
 end
+
 class Repo
   attr_accessor :username, :name, :language, :tests, :clone_url
   def initialize(username, name, clone_url, language)
--- spec/repo_spec.rb
+++ spec/repo_spec.rb
@@ -18,15 +18,31 @@ describe 'Repo' do
 end
 
 describe 'Repo Stats' do
-  it "generates stats for a set of repositories" do
+  let(:stats) {
     repos = [
       Repo.new('mattdsteele', 'mattdsteele/repo1', 'clone_url', 'JavaScript'),
       Repo.new('mattdsteele', 'mattdsteele/repo2', 'clone_url', 'Ruby'),
       Repo.new('mattdsteele', 'mattdsteele/repo3', 'clone_url', 'Ruby')
-    ]
-    repos.last.tests = true
+  ]
+  repos.last.tests = true
+  Repo.stats_for repos
+  }
 
-    stats = Repo.stats_for repos
-    stats.all_languages.should == (1/3)
+  it "generates stats for a set of repositories" do
+    stats.all_languages.should == (1/3.0)
+  end
+
+  it "returns the distinct languages" do
+    stats.languages.should == ['JavaScript', 'Ruby']
+  end
+
+  it "gets stats for a given language" do
+    stats.language('Ruby').should == (1 / 2.0)
+  end
+
+  it "prints out statistics" do
+    results = stats.print_stats
+    results.should include("JavaScript: 0%")
+    results.should include("Ruby: 50%")
   end
 end
