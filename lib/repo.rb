d50c6383355e1a9bfb0911235da9caae9b0657d8
Matt Steele <orphum@gmail.com>
2012-09-16 18:14:42 -0500

    Add basic stats across a set of repos

--- lib/repo.rb
+++ lib/repo.rb
@@ -1,3 +1,11 @@
+class Stats
+  def initialize(repos)
+    @repos = repos
+  end
+  def all_languages
+    @repos.select {|i| i.tests}.length / @repos.length
+  end
+end
 class Repo
   attr_accessor :username, :name, :language, :tests, :clone_url
   def initialize(username, name, clone_url, language)
@@ -5,6 +13,10 @@ class Repo
     @name = name
     @language = language
     @clone_url = clone_url
+  end
+
+  def self.stats_for(repos)
+    Stats.new repos
   end
 end
 
--- spec/repo_spec.rb
+++ spec/repo_spec.rb
@@ -16,3 +16,17 @@ describe 'Repo' do
     repo.tests.should be_true
   end
 end
+
+describe 'Repo Stats' do
+  it "generates stats for a set of repositories" do
+    repos = [
+      Repo.new('mattdsteele', 'mattdsteele/repo1', 'clone_url', 'JavaScript'),
+      Repo.new('mattdsteele', 'mattdsteele/repo2', 'clone_url', 'Ruby'),
+      Repo.new('mattdsteele', 'mattdsteele/repo3', 'clone_url', 'Ruby')
+    ]
+    repos.last.tests = true
+
+    stats = Repo.stats_for repos
+    stats.all_languages.should == (1/3)
+  end
+end
